cf97bf5532041243de5ac73093eb7a657d874faf
Ross <RossMorran@hotmail.co.uk>
2020-05-04 20:19:08 +0100

    Update Random Quote Machine to relect updated Quotes on Design API

--- projects/randomquote/js/index.js
+++ projects/randomquote/js/index.js
@@ -1,23 +1,22 @@
-$(".newQuote").on("click", function () {
-  "use strict";
+"use strict";
 
-  var $quoteInput = $(".quote"),
-      $citationInput = $(".citation");
+$(".newQuote").on("click", () => {
+  const $quoteInput = $(".quote"),
+        $citationInput = $(".citation");
 
   // make an API request to get a randomly generated quote
   $.ajax({
-    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    url: "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
     // call function if request succeeds
-    success: function success(json) {
-      // deal with lsep issue - seen on quote from Apple
-      var quote = json[0].content;
-      var author = json[0].title;
+    success: (json) => {
+      const quote = json[0].content.rendered;
+      const author = json[0].title.rendered;
 
       $quoteInput.html(quote);
       $citationInput.html(author);
 
-      var tweet = $quoteInput.text().trim() + $citationInput.text().trim();
-      var $twtBtn = $(".tweetQuote");
+      const tweet = $quoteInput.text().trim() + $citationInput.text().trim();
+      const $twtBtn = $(".tweetQuote");
 
       // if quote + citation length is 280 chrs or less,
       // enable the tweet quote button
@@ -28,7 +27,7 @@ $(".newQuote").on("click", function () {
       }
     },
     // handle request failure
-    error: function error() {
+    error: () => {
       $quoteInput.html("Error");
       $citationInput.html("Sorry, something went wrong");
     },
@@ -36,28 +35,22 @@ $(".newQuote").on("click", function () {
   });
 });
 
-$(".tweetQuote").on("click", function () {
-  "use strict";
-
-  var tweet = getTweet();
+$(".tweetQuote").on("click", () => {
+  const tweet = getTweet();
   //   handle errors
   if (tweet === false) return;
 
-  var link = "https://twitter.com/home?status=" + tweet;
+  const link = "https://twitter.com/home?status=" + tweet;
   //   give user the option to tweet a quote
   window.open(link);
 });
 
-function getTweet() {
-  "use strict";
-
+function getTweet () {
   //  return the quote + the citation
-
-  var $quote = $(".quote").text(),
-      $citation = $(".citation").text(),
-      defaultMsg = "Click the button to get a quote about design!";
+  const $quote = $(".quote").text(),
+        $citation = $(".citation").text(),
+        defaultMsg = "Click the button to get a quote about design!";
 
   if ($quote === "" || $quote === defaultMsg) return false;
-
-  return encodeURIComponent('"' + $quote.trim() + '" ' + $citation);
+  else return encodeURIComponent('"' + $quote.trim() + '" ' + $citation);
 }
