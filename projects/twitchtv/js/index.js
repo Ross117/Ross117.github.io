b090be5245783731e362b58b63bf17d80e080cfe
Ross <RossMorran@hotmail.co.uk>
2020-05-05 19:32:08 +0100

    Switch proxy server to get around CORs issue

--- projects/twitchtv/js/index.js
+++ projects/twitchtv/js/index.js
@@ -10,7 +10,7 @@ $(window).on("load", function () {
     promisesArr.push(new Promise(function (resolve, reject) {
       $.ajax({
         // use a proxy server to prevent CORS error
-        url: "https://thingproxy.freeboard.io/fetch/https://" + "wind-bow.gomix.me/twitch-api/channels/" + user,
+        url: "https://cors-anywhere.herokuapp.com/https://" + "wind-bow.gomix.me/twitch-api/channels/" + user,
         success: resolve,
         error: reject
       });
@@ -67,7 +67,7 @@ function checkStreamStatus(user) {
   // currently streaming. If they are streaming, get details about the stream.
   $.ajax({
     // use a proxy server to prevent CORS error
-    url: "https://thingproxy.freeboard.io/fetch/https://" + "wind-bow.gomix.me/twitch-api/streams/" + user,
+    url: "https://cors-anywhere.herokuapp.com/https://" + "wind-bow.gomix.me/twitch-api/streams/" + user,
     success: function success(json) {
       if (json.stream === null) {
         html = "<p>Not currently streaming.</p>";
