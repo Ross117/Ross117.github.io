4a17aaa3c9927b2409a9f48dc259062f43158bfc
Ross <rossmorran@hotmail.co.uk>
2021-02-02 22:42:43 +0000

    Re-add link descriptions to Wiki project

--- projects/wiki/js/index.js
+++ projects/wiki/js/index.js
@@ -1,51 +1,56 @@
 // open a random Wikipedia article in a new window
-$(".randomArticle").on("click", function () {
+$(".randomArticle").on("click", () => {
   "use strict";
 
-  var link = "https://en.wikipedia.org/wiki/Special:Random";
+  const link = "https://en.wikipedia.org/wiki/Special:Random";
   window.open(link);
 });
 
-// request search results from the MediaWiki API based on user input
-$(".articleSearch").on("click", function () {
-  "use strict";
-
-  var $userInput = $(".userInput").val();
-
-  $.ajax({
-    // use a proxy server to prevent CORS error
-    url: "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + $userInput,
-    success: function success(json) {
-
-      var $searchResults = $(".searchResults");
-      //an array of titles returned by the API       
-      var titles = json[1];
-      //an array of descriptions returned by the API       
-      var descriptions = json[2];
-      //an array of links returned by the API
-      var links = json[3];
-
-      //clear any existing search results on the page       
-      clearSearchResults();
-
-      // append the links, titles and descriptions of the search results to the page
-      links.map(function (val, index) {
-        $searchResults.append("<div class='card resultContainers'><h5><a href='" + val + "' >" + titles[index] + "</a></h5><p>" + descriptions[index] + "</p></div><br>");
-      });
-    },
-    error: function error() {
-
-      var $searchResults = $(".searchResults");
-      clearSearchResults();
-      $searchResults.append("<p>Sorry, an error has occurred.</p>");
-    }
-  });
-});
-
-//clear any existing search results on the page
 function clearSearchResults() {
   "use strict";
 
-  var $searchResults = $(".searchResults");
   $(".searchResults").html("");
 }
+
+// request search results from the MediaWiki API based on user input
+$(".articleSearch").on("click", () => {
+  "use strict";
+
+  const $userInput = $(".userInput").val();
+
+  $.ajax({
+    // use a proxy server to prevent CORS error
+    url:
+      "https://cors-anywhere.herokuapp.com/http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" +
+      $userInput,
+    success: (data) => {
+      const $searchResults = $(".searchResults");
+      const pageResults = data.query.search;
+
+      //clear any existing search results on the page
+      clearSearchResults();
+
+      // append the links, titles and descriptions of the search results to the page
+      pageResults.forEach((result) => {
+        $searchResults.append(
+          `<div class="card resultContainers"
+            <h5>
+              <a href="https://en.wikipedia.org/wiki/${result.title}" target="_blank">
+                ${result.title}
+              </a>
+            </h5>
+            <p> 
+              ${result.snippet}
+            </p>
+          </div>
+          <br>`
+        );
+      });
+    },
+    error: () => {
+      const $searchResults = $(".searchResults");
+      clearSearchResults();
+      $searchResults.append("<p>Sorry, an error has occurred.</p>");
+    },
+  });
+});
